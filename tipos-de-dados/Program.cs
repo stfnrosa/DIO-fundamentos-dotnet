4451b6ca3caa6ff811374d06af70476e65835030
stfnrosa <rosaa.estefanii@gmail.com>
2024-04-18 17:12:45 -0300

    feat

--- tipos-de-dados/Program.cs
+++ tipos-de-dados/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using tipos_de_dados.Models;
+Pessoa pessoa1 = new Pessoa();
+
+pessoa1.Nome = "Estefani";
+pessoa1.Idade = 23;
+pessoa1.Apresentar();
+
+// Representando a hora atual
+DateTime dataAtual = DateTime.Now;
+Console.WriteLine(dataAtual);
+// Somando horas
+dataAtual = DateTime.Now.AddDays(5);
+Console.WriteLine(dataAtual);
+// Alterando a estrutura 
+Console.WriteLine(dataAtual.ToString("dd/MM/yyyy HH:mm"));
